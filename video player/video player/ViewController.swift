bcd41eac02140d4c35a2fa63125f760cf4f672ad
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-01-09 22:50:01 -0500

    init project

---
+++ video player/video player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video player
+//
+//  Created by camilo andres ibarra yepes on 1/9/19.
+//  Copyright © 2019 camilo andres ibarra yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
