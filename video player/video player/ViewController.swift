8b917a1698c72845ed42348215dd762e2c3a16ff
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-01-09 23:41:43 -0500

    video player

--- video player/video player/ViewController.swift
+++ video player/video player/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func buttonAction(_ sender: Any)
+    {
+        if let path = Bundle.main.path(forResource: "video", ofType: "mp4")
+        {
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:
+            {
+                video.play()
+            })
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
